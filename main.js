e2dd867106be5b98cdb4449dc1f33ec0f49e817b
thomas <thomas@ask-cs.com>
2021-05-07 13:43:08 +0200

    bday text updated

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ if (nightsToJenBirthday > 0) {
   no.innerHTML = `Nope. Jen is not 30 yet. Only ${nightsToJenBirthday} night's sleep. 🌙`;
   console.log(nightsAway(birthdayThomas));
   if (nightsAway(birthdayThomas) === 0) {
-    no.append(" But Thomas is!");
+    no.append(" But Thomas turned 32 today!");
     setGif(partyGifUrl);
   }
 } else {
